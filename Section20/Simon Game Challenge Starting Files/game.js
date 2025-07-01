3dce892425eeacd6da2242f12b73e483337b0bd7
Lu <chloelu727@gmail.com>
2025-07-02 02:21:11 +1000

    Show the Sequence to the User with Animations and Sounds

--- Section20/Simon Game Challenge Starting Files/game.js
+++ Section20/Simon Game Challenge Starting Files/game.js
@@ -8,4 +8,10 @@ function nextSequence() {
   var randonNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randonNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
