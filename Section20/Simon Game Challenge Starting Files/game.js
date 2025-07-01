8ef270ea5f233cef721eb9ff8b138abd03c2b1da
Lu <chloelu727@gmail.com>
2025-07-01 20:33:03 +1000

    Create A New Pattern

--- Section20/Simon Game Challenge Starting Files/game.js
+++ Section20/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,11 @@
-alert("hello");
+//alert("hello");
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  //random from 0-3
+  var randonNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randonNumber];
+  gamePattern.push(randomChosenColour);
+}
